66db025a63620b33ec49f9dc823f815534de1b75
Garrett Jensen <garrett.d.jensen@gmail.com>
2026-08-12 21:27:24 -0600

    Install Google Tag Manager (GTM-THJ7GLP9)
    Adds the GTM head script as the first element in <head> and the noscript
    fallback as the first element in <body>, sitewide via the shared page()
    template so every page picks it up automatically.

--- src/lib/components.js
+++ src/lib/components.js
@@ -388,9 +388,20 @@ function page({ title, description, path, activeSlug, bodyHtml, isHome = false }
   return `<!doctype html>
 <html lang="en">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THJ7GLP9');</script>
+<!-- End Google Tag Manager -->
 ${head({ title, description, path })}
 </head>
 <body class="bg-soft-grey"${isHome ? ' data-page="home"' : ""}>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THJ7GLP9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 ${nav(activeSlug)}
 <main>
 ${bodyHtml}
